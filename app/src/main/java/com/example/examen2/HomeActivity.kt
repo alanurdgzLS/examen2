2ed865876b565e2618cd7333689bd63b05627d3a
alanurdgz <nalan3947@gmail.com>
2021-10-22 13:53:45 -0500

    Login y funcionalidad botón Registrar

---
+++ app/src/main/java/com/example/examen2/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.examen2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+enum class ProviderType{
+    BASIC
+}
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/example/examen2/LoginActivity.kt
+++ app/src/main/java/com/example/examen2/LoginActivity.kt
@@ -1,10 +1,21 @@
 package com.example.examen2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var btnSignUp: Button
+    lateinit var btnLogIn: Button
+    lateinit var txtEmail: EditText
+    lateinit var txtPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -13,5 +24,49 @@ class LoginActivity : AppCompatActivity() {
         val bundle = Bundle()
         bundle.putString("message", "Integración de Firebase Completa")
         analytics.logEvent("InitScreen", bundle)
+
+        setup()
+    }
+
+    private fun initViews(){
+        btnSignUp = findViewById(R.id.btnSignUp)
+        btnLogIn = findViewById(R.id.btnLogIn)
+        txtEmail = findViewById(R.id.emailEditText)
+        txtPassword = findViewById(R.id.passwordEditText)
+    }
+
+    private fun setup(){
+        title="Autenticación"
+        btnSignUp.setOnClickListener {
+            if (txtEmail.text.isNotEmpty() && txtPassword.text.isNotEmpty())
+            {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(txtEmail.text.toString(),
+                    txtPassword.text.toString()).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        }else{
+                            showAlert()
+                        }
+                }
+            }
+        }
+
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se a producido un error autenticando el usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType){
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 }
