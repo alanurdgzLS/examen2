5f027a5a2f90e8a3341cf4df8d925e338501bf2c
alanurdgz <nalan3947@gmail.com>
2021-10-22 11:33:51 -0500

    Integración de Firebase

--- app/src/main/java/com/example/examen2/LoginActivity.kt
+++ app/src/main/java/com/example/examen2/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.example.examen2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase Completa")
+        analytics.logEvent("InitScreen", bundle)
     }
 }
